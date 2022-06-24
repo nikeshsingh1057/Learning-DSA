12beec6a6d9d1c964b610624633794f2fdd63a0f
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-06-24 13:39:06 +0530

    print all the index of the target element in array.

---
+++ Recursion/searchAllOccurance.java
@@ -0,0 +1,45 @@
+//print all the index of the target element in array.
+import java.util.ArrayList;
+import java.util.List;
+
+public class recursion12 {
+    static List<Integer>list=new ArrayList<>(); // taking static list which is outside the main function
+    public static void main(String [] args)
+    {
+        int [] arr={4,5,6,4,3,4,34,4};
+        int i=0,target=4;
+        SearchAllOccurance(arr, target, i);
+        System.out.println(list);
+    }
+    static void SearchAllOccurance(int [] arr,int target,int i)
+    {
+        if(i==arr.length-1)
+        return;
+        if(arr[i]==target)
+        list.add(i);    // adding index to list.
+        SearchAllOccurance(arr, target, ++i);
+
+    }
+}
+
+// by using returning list in avobe we not reutrn list 
+
+public class recursion12 {
+    public static void main(String [] args)
+    {
+        List<Integer>list=new ArrayList<>();
+        int [] arr={4,5,6,4,3,4,34,4};
+        int i=0,target=4;
+        list = SearchAllOccurance(arr, target, i, list); //note use arrayList this will give warning
+        System.out.println(list);
+    }
+    static List SearchAllOccurance(int [] arr,int target,int i,List<Integer> list)
+    {
+        if(i==arr.length-1)
+        return list;
+        if(arr[i]==target)
+        list.add(i);    // adding index to list.
+        return SearchAllOccurance(arr, target, ++i, list);
+
+    }
+}
