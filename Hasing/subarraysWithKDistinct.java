5de9510c4ab26fe619eb9538d740837c548e22e5
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-01-04 19:20:10 +0530

    Update subarraysWithKDistinct.java

--- Hasing/subarraysWithKDistinct.java
+++ Hasing/subarraysWithKDistinct.java
@@ -3,7 +3,7 @@
 class Solution {
     public int subarraysWithKDistinct(int[] arr, int k) {
         
-        return At_Most(arr,k);//- At_Most(arr,k-1);
+        return At_Most(arr,k)- At_Most(arr,k-1);
         
 // logic is we find first at_most k distint substring and than we find at_most k-1 distint integer and than substract we will get aubarray with exactly k element.
 // dry run and find all atmost k substring and than find all atmost k-1 sbustring and then susbstract we will get same answer.
