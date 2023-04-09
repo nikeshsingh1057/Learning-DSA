4095801d8ef78425e6c941dc85cafc72b8197a2e
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-04-09 23:41:54 +0530

    function to check prime number.

---
+++ MathTrickyQuestion/checkPrime.java
@@ -0,0 +1,12 @@
+    // function to check prime no
+
+    public static int prime(int n)
+    {
+        if(n<=1)
+            return 0;
+        
+        for(int i=2;i*i<=n;i++)
+            if(n%i==0)
+                return 0;
+        return 1;
+    }
