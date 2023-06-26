a985db0711ea1ec2dee63928548ca5617235112f
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-06-26 17:16:57 +0530

    convert tree into mirror tree.

---
+++ Tree/Mirror Tree.java
@@ -0,0 +1,20 @@
+// Mirror Tree gfg
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node root) {
+        
+        if(root==null)
+            return;
+        
+       
+        Node temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+           
+        
+        mirror(root.left);
+        mirror(root.right);
+    }
+}
+// we can do also it by using inorder traveshal by putting data in queue and than during popping we swap 
