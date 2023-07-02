aa6c76f59a92ec4b81641cd8b6b9ec517f252273
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-07-02 09:51:34 +0530

    sieve_prime_method

---
+++ MathTrickyQuestion/sieve_prime_method.java
@@ -0,0 +1,35 @@
+package Time_Space_Complixity;
+
+public class sieve_prime_method {
+
+	public static void main(String[] args) {
+		
+		int n=100;
+		primecheck(n);
+
+	}
+	public static void primecheck(int n)
+	{
+		boolean[] prime=new boolean[n+1];
+		
+		prime[0]=prime[1]=true;
+	
+		for(int i=2;i*i<prime.length;i++)
+		{
+			if(prime[i]==false) {
+				
+				for(int k=2;k*i<prime.length;k++) {
+					
+					prime[i*k]=true;
+				}
+			}
+		}
+		for(int i=2;i<prime.length;i++)
+		{
+			if(prime[i]==false)
+				System.out.print(i+" ");
+		}
+		
+	}
+
+}
