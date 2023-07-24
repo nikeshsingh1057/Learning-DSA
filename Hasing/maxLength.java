3d0b66899049eee8f58aa6cd503683247ac1195b
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-07-24 18:15:53 +0530

    Length of the largest subarray with contiguous elements

--- Hasing/maxLength.java
+++ Hasing/maxLength.java
@@ -1,4 +1,4 @@
-// Length of the largest subarray with contiguous elements
+// Length of the largest subarray with contiguous elements  .(isme dublicate nahi hoga testcase me)
 // https://www.codingninjas.com/studio/problems/length-of-the-largest-subarray_2825087?leftPanelTab=0
 
 import java.util.* ;
@@ -22,6 +22,47 @@ public class Solution {
 				min=Math.min(arr[j],min);
 				max=Math.max(arr[j],max);
 
+				if((max-min)==(j-i)){
+					ans=Math.max(max-min+1,ans);
+				}
+			}
+		}
+		return ans;
+	}
+
+}
+
+// Length of the largest subarray with contiguous elements  .(isme dublicate hoga testcase me)
+// above logic not work if testcase contain diblicate so we use hashset to check dublicate and handle answer.
+// https://www.codingninjas.com/studio/problems/length-of-the-largest-subarray-ii_3125966?leftPanelTab=1
+import java.util.* ;
+import java.io.*; 
+ 
+public class Solution {
+
+	public static int maxLength(int[] arr) {
+		
+		int ans=1;
+		int n=arr.length;
+		int min=Integer.MAX_VALUE;
+		int max=Integer.MIN_VALUE;
+
+		for(int i=0;i<n-1;i++){
+			
+			min=arr[i];
+			max=arr[i];
+			HashSet<Integer> hs=new HashSet<>();
+			hs.add(arr[i]);
+
+			for(int j=i+1;j<n;j++){
+
+				if(hs.contains(arr[j]))
+					break;
+
+				min=Math.min(arr[j],min);
+				max=Math.max(arr[j],max);
+				hs.add(arr[j]);
+				
 				if((max-min)==(j-i)){
 					ans=Math.max(max-min+1,ans);
 				}
