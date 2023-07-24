ba75447cad75e95217cd5cadd2a73fcd79d0b9db
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-07-25 00:27:54 +0530

    Longest K unique characters substring

--- Hasing/longestkSubstring.java
+++ Hasing/longestkSubstring.java
@@ -1,4 +1,5 @@
 // Longest K unique characters substring
+// Longest K unique characters substring
 
 class Solution {
     public int longestkSubstr(String s, int k) {
@@ -23,5 +24,54 @@ class Solution {
             }
        }
        return ans;
+    }
+}
+
+// 2nd method
+
+class Solution {
+    public int longestkSubstr(String s, int k) {
+        
+        HashMap<Character,Integer> hs=new HashMap<>();
+        
+        int i=-1;
+        int j=-1;
+        int ans=-1;
+        
+        while(true){
+            
+            boolean f1=false;
+            boolean f2=false;
+            
+            while(i<s.length()-1){
+                f1=true;
+                
+                i++;
+                char ch=s.charAt(i);
+                hs.put(ch,hs.getOrDefault(ch,0)+1);
+                if(hs.size()<k)
+                    continue;
+                else if(hs.size()==k)
+                    ans=Math.max(ans,i-j);
+                else
+                    break;
+            }
+            while(j<i){
+                f2=true;
+                
+                j++;
+                char ch=s.charAt(j);
+                if(hs.get(ch)>1)
+                    hs.put(ch,hs.getOrDefault(ch,0)-1);
+                else if(hs.get(ch)==1)
+                    hs.remove(ch);
+                if(hs.size()==k)
+                    break;
+                
+            }
+            if(f1==false && f2==false)
+                break;
+        }
+        return ans;
     }
 }
