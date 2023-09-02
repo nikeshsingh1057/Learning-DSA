8eb4c0863f8236fcdccbf01402b969dc4fefe2f3
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-09-02 21:39:34 +0530

    largest Sub_tree Sum (Tree hona chaiya See on GfG).

--- Tree/largestSubtreeSum.java
+++ Tree/largestSubtreeSum.java
@@ -47,3 +47,44 @@ public class Solution {
         return a+b+root.data;
     }
 }
+
+// Method 3rd using Pair class See on GfG
+
+class Solution {
+    
+    public static int findLargestSubtreeSum(Node root) {
+        
+        return sum(root).ans;
+    }
+    public static pair sum(Node root){
+        
+        if(root==null)
+            return new pair();
+        
+        pair left=sum(root.left);
+        pair right=sum(root.right);
+        
+        int lrsum = left.p + right.p + root.data; // lrsum=left,right ka sum.
+        
+        pair curr=new pair();    // ye current node ka value set kar rahe hai vo aage evaluete hoga iske upper 
+        
+        int Ans = Get_maxsum(left.ans, right.ans, lrsum);
+        
+        curr.p=lrsum;
+        curr.ans=Ans;    // isme humesa maximum aayega. because isko Get_maxsum se max nikal kar bhejwa rahe hai.
+        
+        return curr;
+    }
+    public static int Get_maxsum(int... arr){
+        
+        int maxsF=arr[0];  // store maximum among all.
+        for(int ele:arr)
+            maxsF=Math.max(maxsF,ele);
+        return maxsF;
+    }
+}
+class pair{
+    
+    int p = 0; // abhi tak me left and right se kitna sum aaya hai usko store kar ke rakhata hai.
+    int ans = -(int)1e9;; // for stroing Largest subtree sum in a tree.
+}
