d64baeb376691a48dfbae2d0c20a0e6ffdec021f
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-09-03 02:20:07 +0530

    Construct Binary Search Tree from Preorder Traversal

--- BinarySearchTree/Construct_BST_FromPreorder.java
+++ BinarySearchTree/Construct_BST_FromPreorder.java
@@ -1,5 +1,7 @@
 // Construct Binary Search Tree from Preorder Traversal. leetcode_1008
 // https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+// method 1st  T.C => O.N(n).
 class Solution {
   
     static int index;
@@ -22,6 +24,34 @@ class Solution {
         TreeNode root = new TreeNode(preorder[index++]);
         root.left = constructBST(preorder, left, root.val);    // passing left range
         root.right = constructBST(preorder, root.val, right);  // passing right range.
+        
+        return root;
+    }
+}
+
+// method 2nd  T.C => O.N(n^2).
+
+class Solution { // time complexity= O.N(n^2).
+    public TreeNode bstFromPreorder(int[] preorder) {
+        
+        TreeNode root=null;
+        
+        for(int i=0;i<preorder.length;i++){
+            
+            root=bst_Preorder(root,preorder[i]);
+        }
+        return root;
+    }
+    public TreeNode bst_Preorder(TreeNode root,int value){
+        
+        if(root==null){
+            root=new TreeNode(value);
+            return root;
+        }
+        if(value<root.val)
+            root.left=bst_Preorder(root.left,value);
+        else
+            root.right=bst_Preorder(root.right,value);
         
         return root;
     }
