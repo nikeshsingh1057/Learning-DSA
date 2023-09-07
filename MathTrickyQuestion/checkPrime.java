9daa569a5ec971b1fa613bcc475e2be68234dfd0
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-09-07 21:44:45 +0530

    function to check prime no

--- MathTrickyQuestion/checkPrime.java
+++ MathTrickyQuestion/checkPrime.java
@@ -3,10 +3,10 @@
     public static int prime(int n)
     {
         if(n<=1)
-            return 0;
+            return 1;
         
         for(int i=2;i*i<=n;i++)
             if(n%i==0)
-                return 0;
-        return 1;
+                return 0;         // 0 for not prime yani composite hai
+        return 1;                 // 1 for prime yani prime hai
     }
