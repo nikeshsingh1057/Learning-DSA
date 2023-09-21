3c83edd0c2298e9219545b89d8784df194fff0f8
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-09-21 19:00:32 +0530

    Merge k Sorted Lists_leet23.

--- ComparableInterFace/ComparatorQuestion/Merge k Sorted Lists_leet23.java
+++ ComparableInterFace/ComparatorQuestion/Merge k Sorted Lists_leet23.java
@@ -32,3 +32,57 @@ public class sorting implements Comparator<ListNode>{
                 return o1.val-o2.val;
     }
 }
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* note iss question me hum comparator interface use kiye hai sort karne ke liye and we also know that ki comparator use karne ke  new class
+me usko implements karna hota hai . jaise upper use kiye hai.
+now hum comprable bhee use kar sakte thee bass hume ListNode ke class me Comparable ko implement karna hota aur compareTo method ko override
+then comprable see bhee PriorityQueue sort ho jata note Comparable interface ko same class me implement karna hota hai jisme object wager 
+banaye hai jaise yaha ListNode me implement karna hoga. */
+
+public class ListNode Comparable<ListNode>{    // ListNode me linked list ka node wagera create ho raha hai i.e sorting linkedlist value.
+    int val;
+    ListNode next;
+    
+    ListNode() {           // ye tino comparator hai.
+    }              
+    ListNode(int val) { 
+        this.val = val; 
+    }
+    ListNode(int val, ListNode next) { 
+        this.val = val; this.next = next; 
+    }
+    
+    public int compareTo(ListNode st) {     // yaha hum compare karwa rahe hai..
+        return this.val-st.val;
+    }
+    public String toString() {  
+		return this.data+" ";
+    }
+}
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        PriorityQueue<ListNode> pq=new PriorityQueue<>();
+        
+        for(ListNode ll: lists){
+            if(ll!=null){
+                pq.add(ll);
+            }
+        }
+        ListNode Dummy=new ListNode();
+        ListNode temp=Dummy;
+        while(!pq.isEmpty()){
+            
+            ListNode rv=pq.poll();
+            Dummy.next=rv;
+            Dummy=Dummy.next;
+            
+            if(rv.next!=null){
+                pq.add(rv.next);
+            }
+        }
+        return temp.next;
+    }
+}
