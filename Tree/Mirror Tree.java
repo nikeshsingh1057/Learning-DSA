84c7b021d5d1a55b2c97b8d1845ae7bafe4ff9a4
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-01-18 12:07:29 +0530

    Mirror Tree or Invert Binary Tree
    https://leetcode.com/problems/invert-binary-tree/submissions/

--- Tree/Mirror Tree.java
+++ Tree/Mirror Tree.java
@@ -19,3 +19,30 @@ class Solution {
     }
 }
 // we can do also it by using inorder traveshal by putting data in queue and than during popping we swap 
+// using level order traveshal Or using Queue method
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            
+            TreeNode ele = q.poll();
+            if(ele==null)
+                continue;
+            TreeNode temp = ele.left;
+            
+            ele.left=ele.right;
+            ele.right=temp;
+            
+            if(ele.left!=null)
+                q.add(ele.left);
+            
+            if(ele.right!=null);
+                q.add(ele.right);
+        }
+        return root;
+    }
+}
