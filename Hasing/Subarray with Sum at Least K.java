d065ef574726ca2ca5aafaf5a2ee1674edb1c948
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-02-04 20:25:18 +0530

    Sortest Subarray with Sum at Least K.

---
+++ Hasing/Subarray with Sum at Least K.java
@@ -0,0 +1,32 @@
+// Shortest Subarray with Sum at Least K
+
+class Solution {
+    public int shortestSubarray(int[] arr, int k) {
+        
+        int ans=Integer.MAX_VALUE;
+        Long sum=0l;
+
+        TreeMap<Long,Integer> map=new TreeMap<>();
+
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i]>=k) return 1;
+
+            sum += arr[i];
+            if(sum>=k) 
+                ans=Math.min(ans,i+1);
+            Long rem=sum-k;
+
+            Map.Entry<Long,Integer> entry = map.floorEntry(rem);
+            while(entry!=null){
+
+                int idx=entry.getValue();
+                ans=Math.min(ans,i-idx);
+                map.remove(entry.getKey());  // very imp line-> agar ussase index se answer mil gaya then remove kar do to avoid tle.
+                entry=map.floorEntry(entry.getKey()-1);
+            }
+            map.put(sum,i);     // prefix sum ko key ke roop me dal rahe hai
+        }
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }
+}
