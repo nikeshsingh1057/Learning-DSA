cbe339380b5452836784ed767cc704a406cc8ed4
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-03-27 11:22:36 +0530

    Find shortest safe route in a matrix
    https://www.geeksforgeeks.org/problems/find-shortest-safe-route-in-a-matrix/1

--- Graph/DFS Based Que/safe route in a matrix.java
+++ Graph/DFS Based Que/safe route in a matrix.java
@@ -1,53 +1,99 @@
 // https://www.geeksforgeeks.org/problems/find-shortest-safe-route-in-a-matrix/1
 // Find shortest safe route in a matrix
 
+// without dp.
 class Solution {
-   int max=1000000000;
+    int max=800000;
     boolean checkSideMine(int i,int j, int mat[][]){
         
-        if(i-1>=0 && mat[i-1][j]==0)return false;
-        if(i+1<mat.length && mat[i+1][j]==0)return false;
-        if(j+1<mat[0].length && mat[i][j+1]==0)return false;
-        if(j-1 >=0 && mat[i][j-1]==0)return false;
+        if(i-1>=0 && mat[i-1][j]==0) return false;   
+        if(i+1<mat.length && mat[i+1][j]==0) return false;
+        if(j+1<mat[0].length && mat[i][j+1]==0) return false;
+        //if(j-1 >=0 && mat[i][j-1]==0)return false; redundent hai no need to check left becuase right move kar rahe hai hamesha.
         return true;
     }
 
-   int f(int i,int j,int mat[][],boolean vis[][]){
+   public int dfs(int i,int j,int mat[][]){
         
-        if(i<0 || j<0 || i>=mat.length|| j>=mat[0].length|| vis[i][j]==true || mat[i][j]==0 )return max;
-        
-        if(checkSideMine(i,j,mat)==false){
+        if(i<0 || j<0 || i>=mat.length|| j>=mat[0].length||  mat[i][j]==-1 || mat[i][j]==0 )
             return max;
-        }
         
-        if(j==0)  return mat[i][0]; 
+        if(checkSideMine(i,j,mat)==false)
+            return max;
         
-        vis[i][j]=true;
+        if(j==mat[0].length-1) return 1; // column reach at end
         
-          int left=mat[i][j]+f(i,j-1,mat,vis);
-          int up=mat[i][j]+f(i-1,j,mat,vis);
-          int down=mat[i][j]+f(i+1,j,mat,vis);
+        int orig=mat[i][j];  // current value orig me put kar diye
         
-       /* here we dont go right bcz it doesnt mean to go again back 
-        if there is no way means no way  */
+        mat[i][j]=-1;   // isko mark -1 kar diye so that dubara usi cell par nahi jaye(to avoid stack over flow)
         
-         vis[i][j]=false;
+         int right = 1+dfs(i,j+1,mat);
+         int up = 1+dfs(i-1,j,mat);
+         int down = 1+dfs(i+1,j,mat);
         
-        int min=Math.min(up,Math.min(down ,left));
+        mat[i][j]= orig; // backtrack karenge to original wala value add kar denge. because to check furter path
+        int min = Math.min(right,Math.min(up,down));
         return min;
-        
    }
     public  int findShortestPath(int[][] mat) {
+        
         int n=mat.length;
-        int m=mat[0].length;
-        boolean vis[][]=new boolean [n][m];
-        
-    
-        
         int min=max;
-        for(int i=0;i<n;i++){
-            min=Math.min(min,f(i,m-1,mat,vis));
-        }
+       
+        for(int i=0;i<n;i++) // row par loop lagaye hai to that har eak row chcek ho sake
+            min=Math.min(min,dfs(i,0,mat));
+        return min==max?-1:min;
+    }
+}
+
+// after memoziation or dp
+
+class Solution {
+    int max=800000;
+    boolean checkSideMine(int i,int j, int mat[][]){
+        
+        if(i-1>=0 && mat[i-1][j]==0) return false;   
+        if(i+1<mat.length && mat[i+1][j]==0) return false;
+        if(j+1<mat[0].length && mat[i][j+1]==0) return false;
+        //if(j-1 >=0 && mat[i][j-1]==0)return false; redundent hai no need to check left becuase right move kar rahe hai.
+        return true;
+    }
+
+   public int dfs(int i,int j,int mat[][],int[][] dp){
+        
+        if(i<0 || j<0 || i>=mat.length|| j>=mat[0].length||  mat[i][j]==-1 || mat[i][j]==0 )
+            return max;
+        
+        if(checkSideMine(i,j,mat)==false)
+            return max;
+        
+        if(dp[i][j] !=-1)
+            return dp[i][j];
+        if(j==mat[0].length-1) return 1; // column reach at end
+        
+        int orig=mat[i][j];  // current value orig me put kar diye
+        
+        mat[i][j]=-1;   // isko mark -1 kar diye so that dubara usi cell par nahi jaye(to avoid stack over flow)
+        
+         int right = 1+dfs(i,j+1,mat,dp);
+         int up = 1+dfs(i-1,j,mat,dp);
+         int down = 1+dfs(i+1,j,mat,dp);
+        
+        mat[i][j]= orig; // backtrack karenge to original wala value add kar denge.
+        int min = Math.min(right,Math.min(up,down));
+        return  dp[i][j]= min;
+   }
+    public  int findShortestPath(int[][] mat) {
+        
+        int n=mat.length;
+        int min=max;
+        int dp[][]=new int[mat.length+1][mat[0].length+1];
+        
+        for(int a[]:dp)
+            Arrays.fill(a,-1);
+            
+        for(int i=0;i<n;i++)
+            min=Math.min(min,dfs(i,0,mat,dp));
         return min==max?-1:min;
     }
 }
