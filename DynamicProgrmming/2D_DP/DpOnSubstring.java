b5128fbc8a712387067aca8073d58e9fd0fdc565
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-04-06 23:13:48 +0530

    Dp  On String
    Imp dp on string question list
    based on lis ,lcs

--- DynamicProgrmming/2D_DP/DpOnSubstring.java
+++ DynamicProgrmming/2D_DP/DpOnSubstring.java
@@ -89,3 +89,37 @@ class Solution {   // dp seen
 
 //3 longest common subsequence
 //4 longest palendromic subsequence.
+
+//5 Shortest Common Supersequence (gfg par only count print karwan hai but leetcode par string print karwan hai.) 
+// question 5 is variation of question LCS
+class Solution       //(isme sirf count print karwa rahe hai)
+{
+    //Function to find length of shortest common supersequence of two strings.
+    public static int shortestCommonSupersequence(String s1,String s2,int m,int n)
+    {
+        int dp[][]=new int[m+1][n+1];
+        
+        for(int a[]:dp)
+            Arrays.fill(a,-1);
+            
+        int ans=solve(s1,s2,m,n,dp);
+        return ans;
+    }
+    public static int solve(String s1,String s2,int m,int n,int dp[][]){
+        
+        if(m==0|| n==0)
+            return m+n;
+        
+        if(dp[m][n]!=-1)
+            return dp[m][n];
+            
+        int a=0;
+        if(s1.charAt(m-1)==s2.charAt(n-1))
+            a = 1+solve(s1,s2,m-1,n-1,dp);
+        
+        else
+            a = 1 + Math.min(solve(s1,s2,m-1,n,dp),solve(s1,s2,m,n-1,dp));
+        
+        return dp[m][n]=a;
+    }
+}
