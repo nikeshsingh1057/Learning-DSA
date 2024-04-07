a5e4ef676b35629023352da69a7f9a42b47c7dd2
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-04-07 14:27:00 +0530

    Dp On Stock
    These are the 6 problem on dp on stock.

---
+++ DynamicProgrmming/Dp On Stock.java
@@ -0,0 +1,8 @@
+// these are leetcode problems dp on stocks.
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/                       121 
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/                    122
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/                   123
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/description/        188
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/         309
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/  714
