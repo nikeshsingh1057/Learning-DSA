5790da63d402d247e3bab65f8cc50963478eb1bd
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-04-08 23:19:37 +0530

    Sum Of All Substring.java

---
+++ DynamicProgrmming/2D_DP/SumOfAllSubstring.java
@@ -0,0 +1,19 @@
+// Sum of all substrings of a number        (gfg).
+// https://www.geeksforgeeks.org/problems/sum-of-all-substrings-of-a-number-1587115621/1
+
+class Solution
+{
+    //Function to find sum of all possible substrings of the given string.
+    public static long sumSubstrings(String s)
+    {
+        long ans=0,previous=0,mod = 1000000007;
+        
+        for(int i=0;i<s.length();i++){
+            
+            long curr= (previous*10) % mod + (s.charAt(i)-'0')*(i+1) % mod;
+            previous = curr;
+            ans = (ans+curr) % mod;
+        }
+        return ans;
+    }
+}
