6226ebf4842f7c6a84371e4085201a49519682a0
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-04-08 07:41:14 +0530

    Dp On Game. (picking first and last element type question)

--- DynamicProgrmming/2D_DP/Dp On Game.java
+++ DynamicProgrmming/2D_DP/Dp On Game.java
@@ -1,6 +1,11 @@
+                                    Dp On Game. (picking first and last element type question)
+
 //1. Selling Wine
 // https://leetcode.com/discuss/interview-question/1282577/selling-wine-dp-easy-c
 // https://github.com/nikeshsingh1057/Super_150_GLA_2025/blob/master/Lec68/Wine_Problem.java
 
 //2. Stone Game
 // https://leetcode.com/problems/stone-game/
+
+//3. predict the winner                                    
+//https://leetcode.com/problems/predict-the-winner/
