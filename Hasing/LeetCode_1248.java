e8cf29c87df7fce2b02ec3734e087021c5644f6b
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-06-22 12:45:57 +0530

    LeetCode_1248.

--- Hasing/LeetCode_1248.java
+++ Hasing/LeetCode_1248.java
@@ -85,3 +85,36 @@ class Solution {
         return ans;
     }
 }
+
+// method 3rd using sliding window + queue
+
+class Solution {
+
+    public int numberOfSubarrays(int[] nums, int k) {
+        
+        Queue<Integer> oddIndices = new LinkedList<>(); // store only 0 and 1.
+        
+        int subarrays = 0;   //to store answer.
+        int lastPopped = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            
+            // If element is odd, append its index to the list.
+            if (nums[i] % 2 == 1) 
+                oddIndices.offer(i);
+            
+            // If the number of odd numbers exceeds k, remove the first odd index.
+            if (oddIndices.size() > k) 
+                lastPopped = oddIndices.poll();
+            
+            // If there are exactly k odd numbers, add the number of even numbers
+            // in the beginning of the subarray to the result.
+            if (oddIndices.size() == k) {
+                int initialGap = oddIndices.peek() - lastPopped;
+                subarrays += initialGap;
+            }
+        }
+
+        return subarrays;
+    }
+}
