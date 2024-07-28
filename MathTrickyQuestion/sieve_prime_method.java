482c8590711b83fc4a7eac6c2c01516c7a8ddbac
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-07-28 15:12:42 +0530

    sieve_prime_method

--- MathTrickyQuestion/sieve_prime_method.java
+++ MathTrickyQuestion/sieve_prime_method.java
@@ -60,3 +60,42 @@ public class SieveOfEratosthenesSimple {
         }
     }
 }
+
+// ------------------------------------------------------------------------------------------------------------------------------
+
+import java.util.Arrays;
+
+public class OptimizedSieve {
+    public static void main(String[] args) {
+	    
+        int n = 50; // Change this to the desired upper limit
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+	    
+        isPrime[0] = false; // 0 is not a prime number
+        isPrime[1] = false; // 1 is not a prime number
+
+        // Mark all multiples of 2 greater than 2 as non-prime
+        for (int i = 2 * 2; i <= n; i += 2) {  // 2 wala ko alag se kar liye now jitna bhee even aayega i.e 4,6,8 no need to check because 2 se already mark hoga
+            isPrime[i] = false;
+        }
+
+        // Start marking multiples from 3, skipping even numbers
+        for (int i = 3; i * i <= n; i += 2) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += 2 * i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // Output the prime numbers
+        System.out.println("Prime numbers up to " + n + ":");
+
+        for (int i = 2; i <= n; i += 2) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
